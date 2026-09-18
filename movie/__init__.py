a52668837fd058293d208773eca310d801744bc7
NotSumin <notsuminjo@gmail.com>
2026-09-18 16:44:39 +0900

    jsm page

--- movie/__init__.py
+++ movie/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
+
     @app.route('/')
     def index():
         return "flask team project!!"
 
+    @app.route('/jsm')
+    def jsm():
+        return render_template('jsm.html')
     return app
