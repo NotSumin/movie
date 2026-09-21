e9de502a2b4263b0fae7c2e0d81615ac1d54d3bc
NotSumin <notsuminjo@gmail.com>
2026-09-21 15:28:33 +0900

    2026-09-21: Basic main page and navbar

--- movie/__init__.py
+++ movie/__init__.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
+import config
 
+naming_convention = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(column_0_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate()
+
+# 애플리케이션 팩토리
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    @app.route('/')
-    def index():
-        return "flask team project!!"
+    # ORM 초기 설정
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+    from . import models
 
-    @app.route('/hsh')
-    def hsh():
-        return render_template('hsh.html')
+    # 블루프린트 등록
+    from .views import main_views, auth_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(auth_views.bp)
 
-    @app.route('/jsm')
-    def jsm():
-        return render_template('jsm.html')
+    # 필터 등록
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
+
     return app
