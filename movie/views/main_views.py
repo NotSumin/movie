b232c20ab61e8a42f317d01e9cd8e209dc8b95de
yjk1101 <92ujin@naver.com>
2026-09-25 14:24:26 +0900

    fix:메인페이지수정

--- movie/views/main_views.py
+++ movie/views/main_views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 
+from movie import db
 from movie.models import Movie
 
 # Blueprint: 라우팅 함수를 체계적으로 관리
@@ -7,31 +8,20 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    popular_movies = Movie.query.filter(
-        Movie.status == "상영중"
-    ).limit(5).all()
+    popular_movies = Movie.query.filter(Movie.status == "상영중").all()
 
-    # 2. 오직 부귀영화에서만!
-    age_movies = Movie.query.filter(
-        Movie.rating == "15세"
-    ).limit(5).all()
+    desc_movies = Movie.query.order_by(Movie.id.desc()).all()
 
-    # 3. 감성을 채우는 예술
-    long_movies = Movie.query.filter(
-        Movie.runtime >= 140
-    ).limit(5).all()
+    rand_movies = Movie.query.order_by(db.func.random()).all()
 
-    # 4. 가볍게 즐기는 영화
-    short_movies = Movie.query.filter(
-        Movie.runtime < 120
-    ).limit(5).all()
+    asc_movies = Movie.query.order_by(Movie.id.asc()).all()
 
     return render_template(
         "index.html",
         popular_movies=popular_movies,
-        age_movies=age_movies,
-        long_movies=long_movies,
-        short_movies=short_movies
+        desc_movies=desc_movies,
+        rand_movies=rand_movies,
+        asc_movies=asc_movies
     )
 
 # 마이페이지 임시 라우트
